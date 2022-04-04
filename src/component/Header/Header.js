df4b7af9bfe6739347cb3ce576cf932ec41421ca
TaHasib981 <tahaisb981@gmail.com>
2022-04-04 18:55:42 +0600

    nav links done

---
+++ src/component/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='nav-container drop-shadow-lg '>
+            <div className='flex justify-center gap-16 py-5 text-2xl text-lime-700'>
+                <Link to="/home"></Link>
+                <Link to="/home">Home</Link>
+                <Link to="/reviews">Reviews</Link>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/blogs">Blogs</Link>
+                <Link to="/about">About</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;

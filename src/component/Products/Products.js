f41a5320119f88eee00d84ddb5c33e71d6fd05aa
TaHasib981 <tahaisb981@gmail.com>
2022-04-05 15:09:38 +0600

    pulished

--- src/component/Products/Products.js
+++ src/component/Products/Products.js
@@ -13,12 +13,12 @@ const Products = () => {
 
             <div className='products-container mt-10'>
                 <div className="product-left px-5 pt-16">
-                    <p className='text-5xl text-yellow-400	' >
+                    <p className='text-5xl text-yellow-400 font-bold' >
 
                         <span>Choose Your Best PowerBank</span><br />
                         <span>as Your Next Useges</span>
                     </p>
-                    <p className='mt-7 px-5 text-lg'>Benefit of portable power banks is their ability to provide stable power in times of need. Everyone is always online these days and most of these devices, like phones, laptops, cameras, etc. work daily, non-stop. Hence, these devices need constant power to keep up with the way the owners use them.
+                    <p className='pBank-dtails mt-7 px-5 text-lg '>Benefit of portable power banks is their ability to provide stable power in times of need. Everyone is always online these days and most of these devices, like phones, laptops, cameras, etc. work daily, non-stop. Hence, these devices need constant power to keep up with the way the owners use them.
                     </p>
 
                     <button className='damo-btn p-8 bg-cyan-400 text-xl flex ml-20 mt-9'>
@@ -33,7 +33,7 @@ const Products = () => {
             {/* customers reviews */}
 
             <div>
-                <p className='mt-20 text-4xl text-green-800'>Customers Reviews</p>
+                <p className='mt-20 text-4xl text-green-800 font-bold'>Customers Reviews</p>
 
                 <div className='feedBack'>
                     {
@@ -45,7 +45,7 @@ const Products = () => {
 
                 </div>
                 <div className='review-link-div' >
-                   <Link className='review-link' to="/reviews">show all button</Link>
+                   <Link className='review-link' to="/reviews">show all reviews</Link>
                 </div>
 
             </div>
